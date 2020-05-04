a3e484b9a2b0bb782e93d55b7e21e6ee693bb9da
ophir <ophirgal2@gmail.com>
2020-05-04 18:11:07 -0400

    got a basic map going

---
+++ server_v2.js
@@ -0,0 +1,24 @@
+const util = require('util')
+const fs = require('fs')
+const readFile = util.promisify(fs.readFile)
+const express = require('express')
+const cors = require('cors')
+
+var app = express()
+app.use(cors()) // enable cors
+app.use(express.static('.'))
+
+app.get('/', function(req, res){
+  readFile("data/IPEDS_data_extended.json")
+  .then(raw  => {
+    let jsonTuples = JSON.parse(raw)
+    res.send(jsonTuples)
+  })
+  .catch( e => { console.log(e) })
+})
+
+app.post
+
+app.listen(8080, function() {
+  console.log("Final Project Data Server is running at localhost: 8080")
+})
--- static/js/map.js
+++ static/js/map.js
@@ -1,5 +1,3 @@
-var idToTitle, titleToId // declare here so it has global scope
-const TOTAL_NUM_OF_USERS = 610  // number of users in the rating data
 submitForm()  // Submit form to immediately generate viz upon webpage load
 
 /*
@@ -72,62 +70,8 @@ function displayNumUsers(value) {
   document.getElementById('numUsersDisplay').innerHTML = value
 }
 
-// =================== code for auto-complete movie search ====================
-var movieInput = document.getElementById("autocomplete-movieInput")
-var results, movies_to_show = []
-var autocomplete_results = document.getElementById("autocomplete-results")
+// =================== code for handling state selection ====================
 
-function autocomplete(val) {
-  var movies_returned = []
-  for (let key in idToTitle) {
-    let movieTitle = idToTitle[key].title
-    // if matches movie title 
-    if (movieTitle.toLowerCase().includes(val.toLowerCase())) { 
-      movies_returned.push(movieTitle)
-    }
-  }
-  return movies_returned
-}
-
-// events
-movieInput.onkeyup = function(e) {
-  input_val = this.value
-
-  if (input_val.length >= 3  && input_val !== "the" && input_val !== "the ") {
-    autocomplete_results.innerHTML = ""
-    movies_to_show = autocomplete(input_val)
-
-    for (i = 0; i < movies_to_show.length; i++) {
-      autocomplete_results.innerHTML +=
-        '<li class="list-item">' + movies_to_show[i] + '</li>'
-    }
-    autocomplete_results.style.display = "block"
-  } else {
-    movies_to_show = []
-    autocomplete_results.innerHTML = ""
-  }
-}
-
-// Add a click listener to auto-compete results list
-autocomplete_results.addEventListener("click", function(e) {
-    // e.target is the clicked element
-    let selectedTitle = e.target.innerHTML
-    if (e.target && e.target.nodeName == "LI") { // If it was a list item
-      // List item found!  Output the value and add it to selected movies!
-      let listItemToAdd = '<li class="selected-movie">' + selectedTitle + '</li>'
-      let selectedMovies = document.getElementById('selectedMovies')
-      if (!selectedMovies.innerHTML.includes(selectedTitle)) {
-        selectedMovies.innerHTML = listItemToAdd + selectedMovies.innerHTML
-      }
-      movieInput.value = ""
-      autocomplete_results.innerHTML = "" // empty the dropdown menu
-    }
-})
-
-// Add a mouseleave listener to auto-compete results list
-autocomplete_results.addEventListener("mouseleave", function(e) {
-  autocomplete_results.style.display = "none"
-})
 
 /*
  * ============================================================================
@@ -161,7 +105,7 @@ function getLinkColor(node, link) {
  */
 function renderMapViz() {
   // Remove potential pre-existing chart
-  document.getElementById("viz").innerHTML = "";
+  document.getElementById("viz").innerHTML = "Hey";
 
   // Define width and height for SVG/visualization
   var width = window.innerWidth/1.1 * 0.75
@@ -184,10 +128,10 @@ function renderMapViz() {
 
       
   // Define linear scale for output
-  var color = d3.scaleLinear()
+  var color = d3.scale.linear()
           .range(["rgb(213,222,217)","rgb(69,173,168)","rgb(84,36,55)","rgb(217,91,67)"]);
 
-  var legendText = ["Cities Lived", "States Lived", "States Visited", "Nada"];
+  var legendText = ["Schools", "...", "Selected States", "..."];
           
   // Append Div for tooltip to SVG
   var div = d3.select("body")
@@ -196,12 +140,12 @@ function renderMapViz() {
           .style("opacity", 0);
 
   // Load in my states data!
-  d3.csv("data/stateslived.csv", function(data) {
+  d3.csv("https://raw.githubusercontent.com/Ophir-Gal/unipick/master/data/stateslived.csv", function(data) {
   color.domain([0,1,2,3]); // setting the range of the input data
-
+  
   // Load GeoJSON data and merge with states data
-  d3.json("https://raw.githubusercontent.com/shawnbot/topogram/master/data/us-states.geojson", function(json) {
-
+  d3.json("https://gist.githubusercontent.com/michellechandra/0b2ce4923dc9b5809922/raw/a476b9098ba0244718b496697c5b350460d32f99/us-states.json", function(json) {
+  
   // Loop through each state data value in the .csv file
   for (var i = 0; i < data.length; i++) {
 
@@ -250,24 +194,24 @@ function renderMapViz() {
 
       
   // Map the cities I have lived in!
-  d3.csv("../../data/cities-lived.csv", function(data) {
-
-  svg.selectAll("circle")
+  d3.csv("https://raw.githubusercontent.com/Ophir-Gal/unipick/master/data/cities-lived.csv", function(data) {
+  
+  svg.append("g")
+    .attr("class", "nodes")
+    .selectAll("use")
     .data(data)
     .enter()
-    .append("circle")
-    .attr("cx", function(d) {
+    .append("use")
+    .attr("href", "#school-icon-g")
+    .attr("transform", "translate(-10,-10)")
+    .attr("x", function(d) {
       return projection([d.lon, d.lat])[0];
     })
-    .attr("cy", function(d) {
+    .attr("y", function(d) {
       return projection([d.lon, d.lat])[1];
     })
-    .attr("r", function(d) {
-      return Math.sqrt(d.years) * 4;
-    })
-      .style("fill", "rgb(217,91,67)")	
-      .style("opacity", 0.85)	
-
+    .style("fill", "rgb(217,91,67)")	
+    .style("opacity", 0.85)	
     // Modification of custom tooltip code provided by Malcolm Maclean, "D3 Tips and Tricks" 
     // http://www.d3noob.org/2013/01/adding-tooltips-to-d3js-graph.html
     .on("mouseover", function(d) {      
@@ -278,13 +222,12 @@ function renderMapViz() {
             .style("left", (d3.event.pageX) + "px")     
             .style("top", (d3.event.pageY - 28) + "px");    
     })   
-
-      // fade out tooltip on mouse out               
-      .on("mouseout", function(d) {       
-          div.transition()        
-            .duration(500)      
-            .style("opacity", 0);   
-      });
+    // fade out tooltip on mouse out               
+    .on("mouseout", function(d) {       
+        div.transition()        
+          .duration(500)      
+          .style("opacity", 0);   
+    });
   });  
           
   // Modified Legend Code from Mike Bostock: http://bl.ocks.org/mbostock/3888852
@@ -292,6 +235,7 @@ function renderMapViz() {
               .attr("class", "legend")
             .attr("width", 140)
             .attr("height", 200)
+            .attr("transform", "translate(270,230)")
             .selectAll("g")
             .data(color.domain().slice().reverse())
             .enter()
